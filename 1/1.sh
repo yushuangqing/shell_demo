5a526ec5a3c3481a09335b5efe016979c438c682
yushuangqing <1172006728@qq.com>
2019-08-21 18:53:30 +0800

    第二次提交

--- 1/1.sh
+++ 1/1.sh
@@ -1,24 +1,35 @@
-read str2
-while [[ "$str2" != "exit" && "$str2" != "quit" ]];do
-	if [ "$str2" == "lower" ];then
-		echo "please input a string:"
-		typeset -l str1
-		read str1
-		echo "$str1"
-		read str2
-	elif [ "$str2" == "upper" ];then
-		echo "please input a string:"
-		typeset -u str1
-		read str1
-		echo "$str1"
-		read str2
-	else
-		num=0
-		echo "please input a string:"
-		typeset -l str1
-		read str1
-		echo "$str1"
-		echo $str1 | sed 's/ //g' | wc -L
-		read str2
-	fi
+echo "input upper; loewer; exit/quit; other"
+read str1
+while [ 1 ];do
+	case $str1 in
+		upper)
+			while [ 1 ];do
+				read str1 
+				if [[ "$str1" == "lower" || "$str1" == "exit" || "$str1" == "quit" ]];then
+					break
+				else
+					echo $(echo $str1 | tr '[a-z]' '[A-Z]')
+				fi
+			done
+			;;
+		lower)
+			while [ 1 ];do
+				read str1 
+				if [[ "$str1" == "upper" || "$str1" == "exit" || "$str1" == "quit" ]];then
+					break
+				else
+					echo $(echo $str1 | tr '[A-Z]' '[a-z]')
+				fi
+			done
+			;;
+		exit)
+			exit ;;
+		quit)
+			exit ;;
+		*)
+			
+			echo $(echo $str1 | tr '[A-Z]' '[a-z]')
+			echo $str1 | sed 's/ //g' | wc -L 
+			read str1 ;;
+	esac
 done
